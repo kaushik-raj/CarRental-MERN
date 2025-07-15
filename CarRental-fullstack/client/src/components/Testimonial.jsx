5c79fc411e8d331d669b4bead0b2ef80eb04351e
Kaushik_raj <kaushik.golu.raj6251@gmail.com>
2025-07-15 13:29:53 +0530

    Some changes

--- CarRental-fullstack/client/src/components/Testimonial.jsx
+++ CarRental-fullstack/client/src/components/Testimonial.jsx
@@ -6,18 +6,18 @@ import { motion } from 'motion/react';
 const Testimonial = () => {
 
     const testimonials = [
-        { name: "Emma Rodriguez", 
-          location: "Barcelona, Spain", 
+        { name: "Anshika ", 
+          location: "Delhi , India", 
           image: assets.testimonial_image_1, 
           testimonial: "I've rented cars from various companies, but the experience with CarRental was exceptional." 
         },
-        { name: "John Smith", 
-          location: "New York, USA", 
+        { name: "Ishaan ", 
+          location: "Pune , India", 
           image: assets.testimonial_image_2, 
           testimonial: "CarRental made my trip so much easier. The car was delivered right to my door, and the customer service was fantastic!" 
         },
-        { name: "Ava Johnson", 
-          location: "Sydney, Australia", 
+        { name: "Aditi", 
+          location: "Mumbai, India", 
           image: assets.testimonial_image_1, 
           testimonial: "I highly recommend CarRental! Their fleet is amazing, and I always feel like I'm getting the best deal with excellent service." 
         }
